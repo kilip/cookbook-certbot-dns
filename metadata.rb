5af98c86cad904aba761c4df92e12cbf3b0fd774
Anthonius Munthi <me@itstoni.com>
2020-02-16 12:53:30 +0800

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,10 @@
-name 'certbot'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures certbot'
+name 'certbot-dns'
+maintainer 'Anthonius Munthi'
+maintainer_email 'me@itstoni.com'
+license 'MIT'
+description 'Creates letsencrypt certificate with dns challenges'
 version '0.1.0'
 chef_version '>= 14.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/certbot/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/certbot'
+issues_url 'https://github.com/kilip/cookbook-certbot-dns/issues'
+source_url 'https://github.com/kilip/cookbook-certbot-dns'
